5b333106e9443cd111a264513a75ec3eda401ff6
JEFERSON DIONEI SEHNEM <56743554+Jeferson100@users.noreply.github.com>
2026-09-16 09:50:59 -0300

    mudando modelo para nvidia/nemotron-3.5-lightning-30b-a3b
    mudando modelo para nvidia/nemotron-3.5-lightning-30b-a3b

--- codigos_rodando/atualizando_readme_tabela.py
+++ codigos_rodando/atualizando_readme_tabela.py
@@ -65,7 +65,7 @@ Develop an objective commentary on the portfolio's performance, analyzing:
 
 PROMPT_FORMATADO = PROMPT.format(carteira=carteira_atual_markdown, variacao_carteira_porcentagem=variacao_carteira_porcentagem)
 
-llm = ChatNVIDIA(model="nvidia/nemotron-3-nano-30b-a3b")
+llm = ChatNVIDIA(model="nvidia/nemotron-3.5-lightning-30b-a3b")
 
 response = llm.invoke([{"role": "user", "content": PROMPT_FORMATADO}])
 
